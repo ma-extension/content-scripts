1b9169da63198b1ed1896ea4527434bc267b5d4b
ronald <ronald-farias@outlook.com>
2019-05-11 16:47:24 -0300

    added manga-livre regex

--- cloud_readers.js
+++ cloud_readers.js
@@ -12,5 +12,11 @@ const CLOUD_READERS = [
         hostname: 'cdmnet.com.br',
         regex_expression: /http:\/\/cdmnet\.com\.br\/titulos\/(?<manga>[a-zA-Z0-9\-\_\%]*)\/manga\/ler-online\/(?<cap>[a-zA-Z0-9\-\_\%]*)#(?<page>[0-9]*)$/,
         icon: 'http://cdmnet.com.br/favicon.ico'
+    },
+    {
+        name: 'manga-livre',
+        hostname: 'mangalivre.com',
+        regex_expression: /https:\/\/mangalivre\.com\/manga\/(?<manga>[a-zA-Z0-9\-\_\%]*)\/[0-9]*/(?<cap>[a-zA-Z0-9\-\_\%]*)#(?<page>[0-9]*)$/,
+        icon: 'https://mangalivre.com/assets/images/favicon.png'
     }
 ]
