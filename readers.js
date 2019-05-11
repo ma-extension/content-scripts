1a02c2815ae9f13670b6d27578803109c0e22eaf
ronald <ronald-farias@outlook.com>
2019-05-11 10:13:35 -0300

    added cloud readers

---
+++ readers.js
@@ -0,0 +1,16 @@
+const CLOUD_READERS = [
+    {
+        name: 'onepiece-ex',
+        hostname: 'onepieceex.net',
+        regex_expression: /https:\/\/onepieceex\.net\/mangas\/leitor\/(?<cap>[a-zA-Z0-9\-\_\%]*)\/#(?<page>[0-9]*)/,
+        exclusive_for: 'One Piece',
+        icon: 'https://onepieceex.net/favicon/favicon-32x32.png'
+
+    },
+    {
+        name: 'central-de-mangas',
+        hostname: 'cdmnet.com.br',
+        regex_expression: /http:\/\/cdmnet\.com\.br\/titulos\/(?<manga>[a-zA-Z0-9\-\_\%]*)\/manga\/ler-online\/(?<cap>[a-zA-Z0-9\-\_\%]*)#(?<page>[0-9]*)$/,
+        icon: 'http://cdmnet.com.br/favicon.ico'
+    }
+]
